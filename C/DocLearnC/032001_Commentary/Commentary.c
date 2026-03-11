7a92fb9c8bbfdc34aabba73925c09d828ec3642d
LimktaN <qz2012@naver.com>
2026-03-11 23:23:31 +0900

    test: change test

---
+++ C/DocLearnC/032001_Commentary/Commentary.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	// One line commentary
+	printf("Hello, Workld!\n");
+
+	/*
+	Multiple line commentary
+	Multiple line commentary
+	Multiple line commentary
+	*/
+	return 0;
+}
