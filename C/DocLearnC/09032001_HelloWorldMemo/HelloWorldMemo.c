dd8116b91ee0a668d56c5417cf1808662ab9b94d
LimktaN <qz2012@naver.com>
2026-03-11 16:26:57 +0900

    test: verify .gitignore policy for VS project artifacts

---
+++ C/DocLearnC/09032001_HelloWorldMemo/HelloWorldMemo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello, World!\n"); // one line memo
+
+	/*
+	
+	multiple line memo
+	multiple line memo
+	multiple line memo
+
+	*/
+
+	return 0;
+}
