c7ff50ab01db010135c172a92a60942f5b4a8149
LimktaN <qz2012@naver.com>
2026-03-26 23:11:09 +0900

    feat: study basic operators: arithmetic and assignment

---
+++ C/DocLearnC/051001_SumThreeInts/SumThreeInts.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	// 사용자로부터 세 정수를 입력 받아 총합을 출력.
+	// 사용자 입력이 저장되는 변수 하나와 값을 누적하는 변수
+	// 하나만 사용해서 구현
+
+	int input = 0, total = 0; // 초기화 필수
+	// 초기화하지 않는 경우, 쓰레기 값이 존재해서 원하는 값이 안나올 수 있음
+	// 그 전에 실행할때 에러 발생함
+
+	scanf_s("%d", &input);
+	total += input;
+
+	scanf_s("%d", &input);
+	total += input;
+
+	scanf_s("%d", &input);
+	total += input;
+
+	printf("Total: %d", total);
+
+	return 0;
+}
---
+++ C/DocLearnC/055001_minus/minus.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	// 사용자로부터 두 정수를 입력 받아 뺄셈을 수행하고
+	// 결과를 출력하는 프로그램을 작성
+	// 단, 절대로 뺄셈 연산자를 사용하지 말고 비트 연산자를 이용해 구현.
+
+	int input1 = 0, input2 = 0;
+
+	scanf_s("%d%d", &input1, &input2);
+
+	// input1 += ~input2 + 1;
+
+	input1 += (input2 ^ 0xFFFFFFFF) + 1;
+
+	printf("%d", input1);
+}
